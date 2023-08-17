f3723e8fa76037ddc4d7acdc1e77c8f3530d20e9
JohnMigwi <johnmigwi101@gmail.com>
2023-08-17 23:50:43 +0300

    ns

--- CountDown.js
+++ CountDown.js
@@ -1,9 +1,9 @@
 function countDown(n) {
     if (n === 0) {
-      return [0]; // Base case: Return an array with the single number 0
+      return [0]; 
     } else {
       const numbers = countDown(n - 1); // Recursive call with smaller n
-      numbers.push(n); // Add the current n to the array
+      numbers.push(n); 
       return numbers;
     }
   }
