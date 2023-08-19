12cbf450c63d6e1789968cbac8ec22002b769e57
JohnMigwi <johnmigwi101@gmail.com>
2023-08-19 23:36:21 +0300

    //

--- CountDown.js
+++ CountDown.js
@@ -2,7 +2,7 @@ function countDown(n) {
     if (n === 0) {
       return [0]; 
     } else {
-      const numbers = countDown(n - 1); // Recursive call with smaller n
+      const numbers = countDown(n - 1
       numbers.push(n); 
       return numbers;
     }
